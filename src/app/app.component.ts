06a5645ed4de22fbd6cc98d2b58384ae6774bdd4
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-05-19 11:21:15 -0500

    otro cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appAngular';
+  nombre = 'Rodrigo Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sitio='http://www.google.com';
+  sueldos = [1700,1600,1900];
+  activo = true;
+
+  esActivo(){
+    if(this.activo)
+      return 'Trabajador activo';
+    else 
+      return 'Trabajador inactivo';
+  }
+
+  ultimos3sueldos(){
+    let suma = 0 ;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }
 }
